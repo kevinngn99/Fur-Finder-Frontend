34ddb3088cd1ab19e30dcc65d2c3925bd7bfb3ea
kevinngn99 <kevinnguyen1@ufl.edu>
2020-06-16 01:56:14 -0400

    Implemented JNI for Android

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import kivy
+kivy.require('1.11.1')
+
 from kivy.config import Config
+
+Config.set('graphics', 'width', '375')
+Config.set('graphics', 'height', '812')
+Config.set('graphics', 'resizable', True)
+Config.set('graphics', 'multisamples', '5')
+
 from kivy.core.window import Window
+
+Window.clearcolor = (1, 1, 1, 1)
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
@@ -12,6 +23,12 @@ from custom_carousel import CustomCarousel
 from card_view import Card
 import requests
 import json
+from jnius import cast
+from jnius import autoclass
+
+
+class TextButton(ButtonBehavior, Label):
+    pass
 
 
 class ImageButton(ButtonBehavior, Image):
@@ -43,9 +60,12 @@ class MyApp(App):
         home_screen.add_widget(Label(text='[color=150470]Home Screen', font_name='assets/Inter-SemiBold.ttf', font_size='40sp', markup=True))
 
         carousel = CustomCarousel(direction='right', pos=(0, 100), size=(375, 200), size_hint=(None, None))
-        data = json.loads(requests.get('https://fur-finder.herokuapp.com/api/pets/').text)
-        for dict in data[:10]:
-            card = Card(dict['name'], dict['gender'], dict['image'], dict['breed'], dict['color'], dict['date']).build()
+        #data = json.loads(requests.get('https://fur-finder.herokuapp.com/api/pets/').text)
+        #for dict in data[:10]:
+            #card = Card(dict['name'], dict['gender'], dict['image'], dict['breed'], dict['color'], dict['date']).build()
+            #carousel.add_widget(card)
+        for i in range(10):
+            card = Card('Waffles', 'Male', 'images/corgi.jpg', 'Corgi', 'Gold', 'Lost').build()
             carousel.add_widget(card)
         home_screen.add_widget(carousel)
 
@@ -63,10 +83,10 @@ class MyApp(App):
         screen_manager.add_widget(message_screen)
         screen_manager.add_widget(pin_screen)
 
-        home_button = ImageButton(source='images/home.png', on_press=lambda b: self.home_callback(screen_manager))
-        report_button = ImageButton(source='images/report.png', on_press=lambda b: self.report_callback(screen_manager))
-        message_button = ImageButton(source='images/message.png', on_press=lambda b: self.message_callback(screen_manager))
-        pin_button = ImageButton(source='images/heart.png', on_press=lambda b: self.pin_callback(screen_manager))
+        home_button = TextButton(text='[color=150470][size=24][font=assets/Feather.ttf]', markup=True, on_press=lambda b: self.home_callback(screen_manager))
+        report_button = TextButton(text='[color=150470][size=24][font=assets/Feather.ttf]', markup=True, on_press=lambda b: self.report_callback(screen_manager))
+        message_button = TextButton(text='[color=150470][size=24][font=assets/Feather.ttf]', markup=True, on_press=lambda b: self.message_callback(screen_manager))
+        pin_button = TextButton(text='[color=150470][size=24][font=assets/Feather.ttf]', markup=True, on_press=lambda b: self.pin_callback(screen_manager))
 
         box_layout = BoxLayout(orientation='horizontal', size_hint=(None, None), size=('375dp', '50dp'))
         box_layout.add_widget(home_button)
@@ -80,11 +100,17 @@ class MyApp(App):
         return anchor_layout
 
 if __name__ == '__main__':
-    kivy.require('1.11.1')
-    Config.set('graphics', 'resizable', False)
-    Config.set('graphics', 'multisamples', '5')
-    Window.clearcolor = (1, 1, 1, 1)
-    Window.size = (375, 812)
+    try:
+        PythonActivity = autoclass('org.kivy.android.PythonActivity')
+        currentActivity = cast('android.app.Activity', PythonActivity.mActivity)
+        DisplayMetrics = autoclass('android.util.DisplayMetrics')
+        displayMetrics = DisplayMetrics()
+        currentActivity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics)
+
+        print('The height of this android device is:', displayMetrics.heightPixels)
+        print('The width of this android device is:', displayMetrics.widthPixels)
+    except:
+        print('Not an android device')
     MyApp().run()
 
 #return Label(text='[color=150470]Fur Finder', font_name='DM-Serif-Display-Regular.ttf', font_size='50', markup=True)
