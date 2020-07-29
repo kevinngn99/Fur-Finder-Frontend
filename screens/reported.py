670e1eeba8ffafd77283a93d462850d511be8cb6
darkswordss <joseg047@ufl.edu>
2020-07-29 15:04:39 -0400

    added toggle buttons the goal is to filter the result page

--- screens/reported.py
+++ screens/reported.py
@@ -5,6 +5,7 @@ from kivy.properties import BooleanProperty
 from kivy.lang.builder import Builder
 from kivy.uix.button import Button
 from kivy.clock import Clock
+from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.behaviors import FocusBehavior
 from kivymd.app import MDApp
@@ -78,7 +79,8 @@ class CustomCard(AnchorLayout):
                 instance.size = (dp(value[0] * scale), dp(value[1] * scale))
 
     def add_image(self):
-        self.custom_image = AsyncImage(size_hint=(None, None), pos_hint={'center_x': 0.5, 'center_y': 0.5}, keep_ratio=True, allow_stretch=True, source=None)
+        self.custom_image = AsyncImage(size_hint=(None, None), pos_hint={'center_x': 0.5, 'center_y': 0.5},
+                                       keep_ratio=True, allow_stretch=True, source=None)
         anchor_layout = CustomAnchorLayout(size_hint=(None, None), size=(dp(150), dp(134)))
         layout = CustomStencilView()
         layout.add_widget(self.custom_image)
@@ -115,7 +117,8 @@ class SelectableCard(RecycleDataViewBehavior, CustomCard):
             rv.screen_manager.get_screen('Pet').children[0].date = rv.data[index]['date']
             rv.screen_manager.get_screen('Pet').children[0].gender = rv.data[index]['gender']
             rv.screen_manager.get_screen('Pet').children[0].images = rv.data[index]['images']
-            rv.screen_manager.get_screen('Pet').children[0].location = rv.data[index]['city'] + ', ' + rv.data[index]['state']
+            rv.screen_manager.get_screen('Pet').children[0].location = rv.data[index]['city'] + ', ' + rv.data[index][
+                'state']
             rv.screen_manager.get_screen('Pet').children[0].name = rv.data[index]['name']
             rv.screen_manager.get_screen('Pet').children[0].petid = rv.data[index]['petid']
             rv.screen_manager.get_screen('Pet').children[0].pet_size = rv.data[index]['pet_size']
@@ -139,25 +142,37 @@ class Reported(MDApp):
 
     class Header:
         def create(self):
-            anchor_layout = AnchorLayout(size_hint=(1, 0.1), anchor_x='left', anchor_y='top', padding=(dp(20), dp(20), dp(20), dp(0)))
+            anchor_layout = AnchorLayout(size_hint=(1, 0.1), anchor_x='left', anchor_y='top',
+                                         padding=(dp(20), dp(20), dp(20), dp(0)))
 
-            header = Label(halign='left', valign='top', font_size=sp(20), color=get_color_from_hex('#023b80'), text='[font=assets/Inter-SemiBold.ttf]Reported Pets', markup=True)
+            header = Label(halign='left', valign='top', font_size=sp(20), color=get_color_from_hex('#023b80'),
+                           text='[font=assets/Inter-SemiBold.ttf]Reported Pets', markup=True)
             header.bind(size=header.setter('text_size'))
 
             anchor_layout.add_widget(header)
             return anchor_layout
+
     class Filter:
         def create(self):
-            anchor_layout = AnchorLayout(size_hint=(1, 0.1), anchor_x='left', anchor_y='top', padding=(dp(20), dp(20), dp(20), dp(0)))
+            box_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.1))
 
-            header = Label(halign='left', valign='top', font_size=sp(20), color=get_color_from_hex('#023b80'), text='[font=assets/Inter-SemiBold.ttf]Filter', markup=True)
-            header.bind(size=header.setter('text_size'))
+            # header = Label(halign='left', valign='top', font_size=sp(20), color=get_color_from_hex('#023b80'), text='[font=assets/Inter-SemiBold.ttf]Filter', markup=True)
+            # header.bind(size=header.setter('text_size'))
 
+            foundBtn = ToggleButton(text="Found", group="status")
+            foundBtn.bind(on_press=Reported.RV.Foundbutton)
+            lostBtn = ToggleButton(text="Lost", group="status")
+            lostBtn.bind(on_press=Reported.RV.Lostbutton)
+            catBtn = ToggleButton(text="Cat", group="type")
+            catBtn.bind(on_press=Reported.RV.Catbutton)
+            dogBtn = ToggleButton(text="Dog", group="type")
+            dogBtn.bind(on_press=Reported.RV.Dogbutton)
+            box_layout.add_widget(foundBtn)
+            box_layout.add_widget(lostBtn)
+            box_layout.add_widget(catBtn)
+            box_layout.add_widget(dogBtn)
 
-
-
-            anchor_layout.add_widget(header)
-            return anchor_layout
+            return box_layout
 
     class RV(RecycleView):
         root = ObjectProperty()
@@ -170,6 +185,139 @@ class Reported(MDApp):
             }
             data = s.get(url='https://fur-finder.herokuapp.com/api/pets//', headers=headers).json()
             return data
+
+        def getFoundPetsFromBackend(self):
+            s = requests.Session()
+            s.hooks['response'].append(self.callback)
+            headers = {
+                'Authorization': 'Token 9a5de7d01e1ce563e4a08a862bf68268128d6f87'
+            }
+            data = s.get(url='https://fur-finder.herokuapp.com/api/pets//', headers=headers).json()
+            newdata = []
+            for pet in reversed(data):
+                if (pet['status'] == "Found"):
+                    newdata.append(
+                        {
+                            'age': pet['age'],
+                            'breed': pet['breed'],
+                            'city': pet['city'],
+                            'color': pet['color'],
+                            'date': pet['date'],
+                            'gender': pet['gender'],
+                            'images': pet['images'],
+                            'name': pet['name'],
+                            'petid': pet['petid'],
+                            'pet_size': pet['size'],
+                            'state': pet['state'],
+                            'status': pet['status'].upper(),
+                            'summary': pet['summary'],
+                            'zip': pet['zip']
+                        }
+                    )
+            return newdata
+
+        def getLostPetsFromBackend(self):
+            s = requests.Session()
+            s.hooks['response'].append(self.callback)
+            headers = {
+                'Authorization': 'Token 9a5de7d01e1ce563e4a08a862bf68268128d6f87'
+            }
+            data = s.get(url='https://fur-finder.herokuapp.com/api/pets//', headers=headers).json()
+            newdata = []
+            for pet in reversed(data):
+                if (pet['status'] == "Lost"):
+                    newdata.append(
+                        {
+                            'age': pet['age'],
+                            'breed': pet['breed'],
+                            'city': pet['city'],
+                            'color': pet['color'],
+                            'date': pet['date'],
+                            'gender': pet['gender'],
+                            'images': pet['images'],
+                            'name': pet['name'],
+                            'petid': pet['petid'],
+                            'pet_size': pet['size'],
+                            'state': pet['state'],
+                            'status': pet['status'].upper(),
+                            'summary': pet['summary'],
+                            'zip': pet['zip']
+                        }
+                    )
+            return newdata
+
+        def getCatsFromBackend(self):
+            s = requests.Session()
+            s.hooks['response'].append(self.callback)
+            headers = {
+                'Authorization': 'Token 9a5de7d01e1ce563e4a08a862bf68268128d6f87'
+            }
+            data = s.get(url='https://fur-finder.herokuapp.com/api/pets//', headers=headers).json()
+            newdata = []
+            for pet in reversed(data):
+                if (pet['breed'] == "Cat"):
+                    newdata.append(
+                        {
+                            'age': pet['age'],
+                            'breed': pet['breed'],
+                            'city': pet['city'],
+                            'color': pet['color'],
+                            'date': pet['date'],
+                            'gender': pet['gender'],
+                            'images': pet['images'],
+                            'name': pet['name'],
+                            'petid': pet['petid'],
+                            'pet_size': pet['size'],
+                            'state': pet['state'],
+                            'status': pet['status'].upper(),
+                            'summary': pet['summary'],
+                            'zip': pet['zip']
+                        }
+                    )
+            return newdata
+
+        def getDogsFromBackend(self):
+            s = requests.Session()
+            s.hooks['response'].append(self.callback)
+            headers = {
+                'Authorization': 'Token 9a5de7d01e1ce563e4a08a862bf68268128d6f87'
+            }
+            data = s.get(url='https://fur-finder.herokuapp.com/api/pets//', headers=headers).json()
+            newdata = []
+            for pet in reversed(data):
+                if (pet['breed'] == "Dog"):
+                    newdata.append(
+                        {
+                            'age': pet['age'],
+                            'breed': pet['breed'],
+                            'city': pet['city'],
+                            'color': pet['color'],
+                            'date': pet['date'],
+                            'gender': pet['gender'],
+                            'images': pet['images'],
+                            'name': pet['name'],
+                            'petid': pet['petid'],
+                            'pet_size': pet['size'],
+                            'state': pet['state'],
+                            'status': pet['status'].upper(),
+                            'summary': pet['summary'],
+                            'zip': pet['zip']
+                        }
+                    )
+            return newdata
+
+        def Catbutton(instance):
+            print('The button <%s> is being pressed' % instance.text)
+
+
+        def Dogbutton(instance):
+            print('The button <%s> is being pressed' % instance.text)
+
+        def Foundbutton(instance):
+            print('The button <%s> is being pressed' % instance.text)
+
+        def Lostbutton(instance):
+            print('The button <%s> is being pressed' % instance.text)
 
         def callback(self, r, **kwargs):
             if self.load:
@@ -246,18 +394,15 @@ class Reported(MDApp):
         header = self.Header().create()
         filter = self.Filter().create()
         anchor_layout = AnchorLayout(size_hint=(1, 0.9), padding=(dp(20), dp(0), dp(20), dp(0)))
-        rv = self.RV(smooth_scroll_end=dp(10), root=anchor_layout, screen_manager=screen_manager, size_hint=(1, 1), effect_cls=ScrollEffect, bar_inactive_color=(0, 0, 0, 0), bar_color=(0, 0, 0, 0))
+        rv = self.RV(smooth_scroll_end=dp(10), root=anchor_layout, screen_manager=screen_manager, size_hint=(1, 1),
+                     effect_cls=ScrollEffect, bar_inactive_color=(0, 0, 0, 0), bar_color=(0, 0, 0, 0))
         anchor_layout.add_widget(rv)
         box_layout.add_widget(header)
         box_layout.add_widget(filter)
+
         box_layout.add_widget(anchor_layout)
         rv_screen = Screen(name='RV')
         rv_screen.add_widget(box_layout)
-
-        foundBtn = Button(text="Found", size_hint=(1, None), size=(dp(100), dp(50)))
-        box_layout.add_widget(foundBtn)
-
-
 
         pet_screen = Screen(name='Pet')
         pet_screen.add_widget(Pet(root_sm=self.root_sm).create(screen_manager))
@@ -267,6 +412,5 @@ class Reported(MDApp):
 
         reported_screen = self.CustomScreen(name='Reported')
         reported_screen.add_widget(screen_manager)
-
 
         return reported_screen
